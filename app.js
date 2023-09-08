a08e77400ebd90e9b2e3208435db948d116af495
Thiago Monnay <107085054+tmonnay@users.noreply.github.com>
2023-09-07 22:45:17 -0300

    adicionando commit do ambiente local para o online

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando um sistema de gerenciamanto de cadastros")
-console.log("NOvo commit no arquivo app")
+console.log("Novo commit no arquivo app")
+console.log("enviando o commit para o github")
+
 
